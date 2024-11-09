f1ee9ab396160370c63a458928a6db9ab2bad701
Glitch (glitch-hello-node) <none>
2024-11-09 11:47:46 +0000

    🎍🙎 Checkpoint ./package.json:15785832/7939 ./server.js:15785832/56

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", function(request, response){
 // HTTP Server
 const http = require("http");
 //const hostname = "127.0.0.1";
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 //server.listen(port, hostname, function() {}); 
 server.listen(port, function(){
