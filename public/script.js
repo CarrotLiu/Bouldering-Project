15ef809e1b2aa7cf6d8a062db079db9eea6a2d5d
Glitch (glitch-hello-node) <none>
2025-01-29 20:36:56 +0000

    🎻🛣 Checkpoint ./public/script.js:31616692/2479

--- public/script.js
+++ public/script.js
@@ -49,26 +49,18 @@ let scene;
 
 const challenges=[["Climb with left hand only", "Climb with right hand only", "Climb without hands", "Climb without feet", "Grab every boulder with both hands","Skip three holds of your choice."],["climb with one hand only", "skip two holds of your choice"],["skip one hold of your choice"],["climb while keeping your back towards the wall all the time"] ];
 const challengeColor=["#007944", "#FFE31A", "#F35588", "#80C4E9"];
+const routeColor=["#007944", "#FFE31A", "#F35588", "#80C4E9"];
 let detected = []; 
+
 socket = io.connect();
-let data = {};
-// const challengeW = ["challenge", "climbing", "fun", "game", "interesting"];
-// const routeW = ["random", "randomize", "route", "new", "which"];
-// const restW = ["rest", "tired", "finish", "sent", "congrats"];
-// const friendW = ["friend","buddy", "partner"];
-// const photoW = ["photo", "camera", "picture"];
-// const byeW = ["bye", "goodbye", "see you", "done with today", "home"];
-// const helpW = ["help","stuck",""]
-// const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-// const recognition = new SpeechRecognition();
-// recognition.lang = "en-US";
-// recognition.continuous = true;
-// recognition.interimResults = false;
+let spkdata = {};
+let scndata={};
 
 socket.on('speak', function (data){
-  // console.log(data);
-  // level = data.a;
-  isSpeaking = data.s;
+  isSpeaking = spkdata.s;
+})
+socket.on('scene', function (data){
+  
 })
 
 function preload(){
@@ -449,49 +441,39 @@ function draw() {
   }
   push();
   if(isSpeaking){ //speaking
-    // if(isListening){
-      // stopRecording();
-    // }
     if(windowWidth < 1000){
-      mic.start(); // Start mic only when speaking
-      level = mic.getLevel(); // Get microphone input level
+      mic.start(); 
+      level = mic.getLevel(); 
     }else{
       amplitude.setInput(currentSpeak);
-    level = amplitude.getLevel();
+      level = amplitude.getLevel();
     }
-    if(!data.s){
+    if(!spkdata.s){
       sendSpeakDt();
     }
-    // console.log(level)
-    
-    focusing();
-  }else{ // listening
-    // if(!isListening){
-      // startRecording();
-      // isListening=true;
-    // }
-    if(data.s){
+  }else{ 
+    if(spkdata.s){
       sendSpeakDt();
-      
     }
     if(windowWidth < 1000){
-      mic.stop(); // Start mic only when speaking
-      level = 0; // Get microphone input level
+      mic.stop(); 
+      level = 0; 
     }
-    idling();
-    // sendListenDt();
   }
   pop();
   if(stage == 0){
-    scene = null;
+    if(isSpeaking){//normal stage
+      focusing();
+    }else{
+      idling();
+    }
+  } else if(stage == 1){
     
-  } else if(stage == 1){//intro stage
     scene = null;
   } else if(stage == 2){//challenge stage
     challenging();
   } else if(stage == 3){//find friend
     scene = null;
-    
   }else if(stage == 4){//random route
     
   }else if(stage == 5){//rest timer
@@ -503,26 +485,19 @@ function draw() {
   }else if(stage == 8){//bye
     
   }
-  
-  
 }
 
 function sendSpeakDt(){
   
-    data.s = isSpeaking;
-    // data.l = isListening;
-    data.a = amplitude.getLevel();
-    socket.emit("speak", data);
-    console.log(data);
+    spkdata.s = isSpeaking;
+    spkdata.a = amplitude.getLevel();
+    socket.emit("speak", spkdata);
+    console.log(spkdata);
 }
 
-function sendListenDt(){
-  let data = {};
-  data.s = isSpeaking;
-  data.l = isListening;
-  data.a = amplitude.getLevel();
-  socket.emit("listen", data);
-  // console.log(data.s);
+function sendSceneDt(){
+  scndata.s = isSpeaking;
+  scndata.l = isListening;
 }
 
 
@@ -692,6 +667,18 @@ io
 
 p5 ml5 Stats dat alpha blue brightness color green hue lerpColor lightness red saturation background clear colorMode fill noFill noStroke stroke erase noErase 2D Primitives arc ellipse circle line point quad rect square triangle ellipseMode noSmooth rectMode smooth strokeCap strokeJoin strokeWeight bezier bezierDetail bezierPoint bezierTangent curve curveDetail curveTightness curvePoint curveTangent beginContour beginShape bezierVertex curveVertex endContour endShape quadraticVertex vertex plane box sphere cylinder cone ellipsoid torus loadModel model HALF_PI PI QUARTER_PI TAU TWO_PI DEGREES RADIANS print frameCount deltaTime focused cursor frameRate noCursor displayWidth displayHeight windowWidth windowHeight windowResized width height fullscreen pixelDensity displayDensity getURL getURLPath getURLParams remove disableFriendlyErrors noLoop loop isLooping push pop redraw select selectAll removeElements changed input createDiv createP createSpan createImg createA createSlider createButton createCheckbox createSelect createRadio createColorPicker createInput createFileInput createVideo createAudio VIDEO AUDIO createCapture createElement createCanvas resizeCanvas noCanvas createGraphics blendMode drawingContext setAttributes boolean string number applyMatrix resetMatrix rotate rotateX rotateY rotateZ scale shearX shearY translate storeItem getItem clearStorage removeItem createStringDict createNumberDict append arrayCopy concat reverse shorten shuffle sort splice subset float int str byte char unchar hex unhex join match matchAll nf nfc nfp nfs split splitTokens trim deviceOrientation accelerationX accelerationY accelerationZ pAccelerationX pAccelerationY pAccelerationZ rotationX rotationY rotationZ pRotationX pRotationY pRotationZ turnAxis setMoveThreshold setShakeThreshold deviceMoved deviceTurned deviceShaken keyIsPressed key keyCode keyPressed keyReleased keyTyped keyIsDown movedX movedY mouseX mouseY pmouseX pmouseY winMouseX winMouseY pwinMouseX pwinMouseY mouseButton mouseWheel mouseIsPressed requestPointerLock exitPointerLock touches createImage saveCanvas saveFrames image tint noTint imageMode pixels blend copy filter THRESHOLD GRAY OPAQUE INVERT POSTERIZE BLUR ERODE DILATE get loadPixels set updatePixels loadImage loadJSON loadStrings loadTable loadXML loadBytes httpGet httpPost httpDo Output createWriter save saveJSON saveStrings saveTable day hour minute millis month second year abs ceil constrain dist exp floor lerp log mag map max min norm pow round sq sqrt fract createVector noise noiseDetail noiseSeed randomSeed random randomGaussian acos asin atan atan2 cos sin tan degrees radians angleMode textAlign textLeading textSize textStyle textWidth textAscent textDescent loadFont text textFont orbitControl debugMode noDebugMode ambientLight specularColor directionalLight pointLight lights lightFalloff spotLight noLights loadShader createShader shader resetShader normalMaterial texture textureMode textureWrap ambientMaterial emissiveMaterial specularMaterial shininess camera perspective ortho frustum createCamera setCamera ADD CENTER CORNER CORNERS POINTS WEBGL RGB ARGB HSB LINES CLOSE BACKSPACE DELETE ENTER RETURN TAB ESCAPE SHIFT CONTROL OPTION ALT UP_ARROW DOWN_ARROW LEFT_ARROW RIGHT_ARROW sampleRate freqToMidi midiToFreq soundFormats getAudioContext userStartAudio loadSound createConvolver setBPM saveSound getMasterVolume masterVolume soundOut chain drywet biquadFilter process freq res gain toggle setType pan phase triggerAttack triggerRelease setADSR attack decay sustain release dispose notes polyvalue AudioVoice noteADSR noteAttack noteRelease isLoaded playMode set isPlaying isPaused setVolume getPan rate duration currentTime jump channels frames getPeaks reverseBuffer onended setPath setBuffer processPeaks addCue removeCue clearCues getBlob getLevel toggleNormalize waveform analyze getEnergy getCentroid linAverages logAverages getOctaveBands fade attackTime attackLevel decayTime decayLevel releaseTime releaseLevel setRange setExp width output stream mediaStream currentSource enabled amplitude getSources setSource bands panner positionX positionY positionZ orient orientX orientY orientZ setFalloff maxDist rollof leftDelay rightDelay delayTime feedback convolverNode impulses addImpulse resetImpulse toggleImpulse sequence getBPM addPhrase removePhrase getPhrase replaceSequence onStep musicalTimeMode maxIterations synced bpm timeSignature interval iterations compressor knee ratio threshold reduction record isDetected update onPeak WaveShaperNode getAmount getOversample amp setInput connect disconnect play pause stop start add mult
 */
+// const challengeW = ["challenge", "climbing", "fun", "game", "interesting"];
+// const routeW = ["random", "randomize", "route", "new", "which"];
+// const restW = ["rest", "tired", "finish", "sent", "congrats"];
+// const friendW = ["friend","buddy", "partner"];
+// const photoW = ["photo", "camera", "picture"];
+// const byeW = ["bye", "goodbye", "see you", "done with today", "home"];
+// const helpW = ["help","stuck",""]
+// const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+// const recognition = new SpeechRecognition();
+// recognition.lang = "en-US";
+// recognition.continuous = true;
+// recognition.interimResults = false;
 // const statusElement = document.getElementById("status");
 // const matchedWords = document.getElementById("detectedWords");
 // const startButton = document.getElementById("startButton");
